1f56d0235349cfa9aeb8eba520a477fd1526c0d7
HUD-Software <104791429+HUD-Software@users.noreply.github.com>
2022-09-04 23:29:21 +0200

    Update build.bat

--- Build/CMake/Windows/build.bat
+++ Build/CMake/Windows/build.bat
@@ -6,22 +6,22 @@ SETLOCAL
 :: ======================
 IF /I "%~1" EQU "Visual Studio 2019" (
     CALL %~dp0build_msvc.bat "%~1" "%~2" "%~3" "%~4" "%~5"
-    EXIT %ERRORLEVEL%
+    EXIT /B %ERRORLEVEL%
 )
 
 IF /I "%~1" EQU "Visual Studio 2022" (
     CALL %~dp0build_msvc.bat "%~1" "%~2" "%~3" "%~4" "%~5"
-    EXIT %ERRORLEVEL%
+    EXIT /B %ERRORLEVEL%
 )
 
 IF /I "%~1" EQU "NMake Visual Studio 2019" (
     CALL %~dp0build_nmake_makefile.bat "%~1" "%~2" "%~3" "%~4" "%~5"
-    EXIT %ERRORLEVEL%
+    EXIT %/B ERRORLEVEL%
 )
 
 IF /I "%~1" EQU "NMake Visual Studio 2022" (
     CALL %~dp0build_nmake_makefile.bat "%~1" "%~2" "%~3" "%~4" "%~5"
-    EXIT %ERRORLEVEL%
+    EXIT /B %ERRORLEVEL%
 )
 
 :: ===================================
@@ -34,4 +34,4 @@ ECHO     * Visual Studio 2022
 ECHO     * NMake Visual Studio 2019
 ECHO     * NMake Visual Studio 2022
 ECHO.
-EXIT 1
+EXIT /B 1
