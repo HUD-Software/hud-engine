7fd87de2a65f19bd9fdfe2229e6a1e0d55e22bed
HUD-Software <104791429+HUD-Software@users.noreply.github.com>
2022-09-05 12:37:08 +0200

    Update generate.bat

--- Build/CMake/Windows/generate.bat
+++ Build/CMake/Windows/generate.bat
@@ -7,24 +7,24 @@ SETLOCAL
 where /q cmake
 IF ERRORLEVEL 1 (
 ECHO CMake not found
-EXIT /B 1
+EXIT 1
 )
 
 IF /I "%~1" EQU "Visual Studio 2019" (
     CALL %~dp0generate_msvc.bat "%~1" "%~2" "%~3"
-    EXIT /B %ERRORLEVEL% 
+    EXIT %ERRORLEVEL% 
 )
 IF /I "%~1" EQU "Visual Studio 2022" (
     CALL %~dp0generate_msvc.bat "%~1" "%~2" "%~3"
-    EXIT %/B ERRORLEVEL% 
+    EXIT ERRORLEVEL% 
 )
 IF /I "%~1" EQU "NMake Visual Studio 2019" (
     CALL %~dp0generate_nmake_makefile.bat "%~1" "%~2" "%~3" "%~4"
-    EXIT /B %ERRORLEVEL%
+    EXIT %ERRORLEVEL%
 )
 IF /I "%~1" EQU "NMake Visual Studio 2022" (
     CALL %~dp0generate_nmake_makefile.bat "%~1" "%~2" "%~3" "%~4"
-    EXIT /B %ERRORLEVEL%
+    EXIT %ERRORLEVEL%
 )
 
 
@@ -38,4 +38,4 @@ ECHO     * Visual Studio 2022
 ECHO     * NMake Visual Studio 2019
 ECHO     * NMake Visual Studio 2022
 ECHO.
-EXIT /B 1
+EXIT 1
