6565a436c1869c758ac1671512f00d3ef0f283bf
Julian LALU <julian.lalu@gmail.com>
2022-09-05 10:47:51 +0200

    New tests

--- Build/CMake/Windows/build.bat
+++ Build/CMake/Windows/build.bat
@@ -5,21 +5,25 @@
 :: ======================
 IF /I "%~1" EQU "Visual Studio 2019" (
     CALL %~dp0build_msvc.bat "%~1" "%~2" "%~3" "%~4" "%~5"
+    ECHO. build return %ERRORLEVEL%
     EXIT /B %ERRORLEVEL%
 )
 
 IF /I "%~1" EQU "Visual Studio 2022" (
     CALL %~dp0build_msvc.bat "%~1" "%~2" "%~3" "%~4" "%~5"
+    ECHO. build return %ERRORLEVEL%
     EXIT /B %ERRORLEVEL%
 )
 
 IF /I "%~1" EQU "NMake Visual Studio 2019" (
     CALL %~dp0build_nmake_makefile.bat "%~1" "%~2" "%~3" "%~4" "%~5"
+    ECHO. build return %ERRORLEVEL%
     EXIT /B %ERRORLEVEL%
 )
 
 IF /I "%~1" EQU "NMake Visual Studio 2022" (
     CALL %~dp0build_nmake_makefile.bat "%~1" "%~2" "%~3" "%~4" "%~5"
+    ECHO. build return %ERRORLEVEL%
     EXIT /B %ERRORLEVEL%
 )
 
