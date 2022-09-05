4976ffd8d1fbe01718f05641a29615d4187ef038
Julian LALU <julian.lalu@gmail.com>
2022-09-05 09:35:59 +0200

    Print msbuild result

--- Build/CMake/Windows/build.bat
+++ Build/CMake/Windows/build.bat
@@ -11,6 +11,7 @@ IF /I "%~1" EQU "Visual Studio 2019" (
 
 IF /I "%~1" EQU "Visual Studio 2022" (
     CALL %~dp0build_msvc.bat "%~1" "%~2" "%~3" "%~4" "%~5"
+    ECHO. build return %ERRORLEVEL%
     EXIT %ERRORLEVEL%
 )
 
